2743a664f46f0cf4f4018beff5816fa3eebc6791
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-04 12:53:01 +0800

    Added initial onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,33 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+import { currentUser } from '@clerk/nextjs';
+
 async function Page() {
+	// init user
+	const user = await currentUser();
+
+	// init user info
+	const userInfo = {};
+
+	// init user data
+	const userData = {
+		id: user?.id,
+		objectId: userInfo?._id,
+		username: userInfo?.username || user?.username,
+		name: userInfo?.name || user?.firstName || '',
+		bio: userInfo?.bio || '',
+		image: userInfo?.image || user.imageURL,
+	};
+
 	return (
-		<main>
+		<main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
 			<h1 className='head-text'>Onboarding</h1>
 			<p className='mt-3 text-base-regular text-light-2'>
 				Complete your profile now to use threads
 			</p>
+
+			<section className='mt-9 bg-dark-2 p-10'>
+				<AccountProfile user={userData} btnTitle='Continue' />
+			</section>
 		</main>
 	);
 }
