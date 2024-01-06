47215671c69ed334b68d8e570cb2ca5ccd115f12
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-06 13:44:10 +0800

    Finished adding comment function

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -23,7 +23,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
 				<ThreadCard
 					key={thread._id}
 					id={thread._id}
-					currentUserId={user.id}
+					currentUserId={user?.id || ''}
 					parentId={thread.parentId}
 					content={thread.text}
 					author={thread.author}
@@ -36,9 +36,26 @@ const Page = async ({ params }: { params: { id: string } }) => {
 			<div className='mt-7'>
 				<Comment
 					threadId={thread.id}
-					currentUserImg={user.imageUrl}
+					currentUserImg={userInfo.image}
 					currentUserId={JSON.stringify(userInfo._id)}
 				/>
+			</div>
+
+			<div className='mt-10'>
+				{thread.children.map((childItem: any) => (
+					<ThreadCard
+						key={childItem._id}
+						id={childItem._id}
+						currentUserId={childItem?.id || ''}
+						parentId={childItem.parentId}
+						content={childItem.text}
+						author={childItem.author}
+						community={childItem.community}
+						createdAt={childItem.createdAt}
+						comments={childItem.children}
+						isComment
+					/>
+				))}
 			</div>
 		</section>
 	);
