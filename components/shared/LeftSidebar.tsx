6664587de6db4f5338d0dc74992dbe3b7b1fe038
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-07 13:48:05 +0800

    Finished profile page

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,12 +5,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { usePathname, useRouter } from 'next/navigation';
-import { SignOutButton, SignedIn } from '@clerk/nextjs';
+import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
 
 function LeftSidebar() {
 	// init router
 	const router = useRouter();
 	const pathname = usePathname();
+	// init auth
+	const { userId } = useAuth();
 
 	return (
 		<section className='custom-scrollbar leftsidebar'>
@@ -19,6 +21,8 @@ function LeftSidebar() {
 					const isActive =
 						(pathname.includes(link.route) && link.route.length > 1) ||
 						pathname === link.route;
+
+					if (link.route === '/profile') link.route = `${link.route}/${userId}`;
 
 					return (
 						<Link
