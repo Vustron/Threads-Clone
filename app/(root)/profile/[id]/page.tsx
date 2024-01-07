ba641b82cb971cf4a893d0d1787e35fba89f9bc3
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-07 12:33:17 +0800

    Added initial profile info and tabs

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,9 @@
 import ProfileHeader from '@/components/shared/ProfileHeader';
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs';
+import { profileTabs } from '@/constants';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 const Page = async ({ params }: { params: { id: string } }) => {
@@ -23,6 +26,44 @@ const Page = async ({ params }: { params: { id: string } }) => {
 				imgUrl={userData.image}
 				bio={userData.bio}
 			/>
+
+			<div className='mt-9'>
+				<Tabs defaultValue='threads' className='w-full'>
+					<TabsList className='tab'>
+						{profileTabs.map((tab) => (
+							<TabsTrigger key={tab.label} value={tab.value} className='tab'>
+								<Image
+									src={tab.icon}
+									alt={tab.label}
+									width={24}
+									height={24}
+									className='object-contain'
+								/>
+								<p className='max-sm:hidden'>{tab.label}</p>
+
+								{tab.label === 'Threads' && (
+									<p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+										{userData?.threads?.length}
+									</p>
+								)}
+							</TabsTrigger>
+						))}
+					</TabsList>
+					{profileTabs.map((tab) => (
+						<TabsContent
+							key={`content-${tab.label}`}
+							value={tab.value}
+							className='w-full text-light-1'
+						>
+							<ThreadsTab
+								currentUserId={user.id}
+								accountId={userData.id}
+								accountType='User'
+							/>
+						</TabsContent>
+					))}
+				</Tabs>
+			</div>
 		</section>
 	);
 };
