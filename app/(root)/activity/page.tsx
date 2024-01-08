538250c7f8e954ced9956dc565f123e3a03c708c
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-08 09:28:49 +0800

    Added initial activity section

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,20 @@
+import { fetchUser, fetchUsers, getActivity } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
 const Page = async () => {
+	// fetch current user
+	const user = await currentUser();
+	if (!user) return null;
+
+	const userInfo = await fetchUser(user.id);
+	const userData = JSON.parse(JSON.stringify(userInfo));
+
+	if (!userInfo?.onboarded) redirect('/onboarding');
+
+	// fetch activities
+	const activity = await getActivity(userData._id);
+
 	return (
 		<section>
 			<h1 className='head-text mb-10'>Activity</h1>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -140,3 +140,32 @@ export async function fetchUsers({
 		throw new Error(`Failed to fetch users: ${error.message}`);
 	}
 }
+
+export async function getActivity(userId: string) {
+	try {
+		connectToDB();
+
+		// find all threads created by user
+		const userThreads = await Thread.find({
+			author: userId,
+		});
+
+		// collect all child thread ids
+		const childThreadIds = userThreads.reduce((acc, userThread) => {
+			return acc.concat(userThread.children);
+		});
+
+		const replies = await Thread.find({
+			_id: { $in: childThreadIds },
+			author: { $ne: userId },
+		}).populate({
+			path: 'author',
+			model: User,
+			select: 'name image _id',
+		});
+
+		return replies;
+	} catch (error: any) {
+		throw new Error(`Failed to fetch user posts: ${error.message}`);
+	}
+}
