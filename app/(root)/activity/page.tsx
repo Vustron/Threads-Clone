72b6e1e93b944969c2031e3ed671be4c9c77e7e6
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-08 09:34:27 +0800

    Added initial function on activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,5 +1,7 @@
 import { fetchUser, fetchUsers, getActivity } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 const Page = async () => {
@@ -18,6 +20,28 @@ const Page = async () => {
 	return (
 		<section>
 			<h1 className='head-text mb-10'>Activity</h1>
+
+			<section className='mt-10 flex flex-col gap-5'>
+				{activity.length > 0 ? (
+					<>
+						{activity.map((activity) => (
+							<Link key={activity._id} href={`/thread/${activity.parentId}`}>
+								<article className='activity-card'>
+									<Image
+										src={activity.author.image}
+										alt='Profile picture'
+										width={20}
+										height={20}
+										className='rounded-full object-cover'
+									/>
+								</article>
+							</Link>
+						))}
+					</>
+				) : (
+					<p>No activity yet</p>
+				)}
+			</section>
 		</section>
 	);
 };
