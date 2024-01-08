f1cac4195d3fa681a82680402502ca41a22ece0d
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-08 12:18:42 +0800

    Finished adding community search

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,7 +1,50 @@
+import CommunityCard from '@/components/cards/CommunityCard';
+import UserCard from '@/components/cards/UserCard';
+import { fetchCommunities } from '@/lib/actions/community.actions';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
 const Page = async () => {
+	// fetch current user
+	const user = await currentUser();
+	if (!user) return null;
+
+	const userInfo = await fetchUser(user.id);
+	const userData = JSON.parse(JSON.stringify(userInfo));
+
+	if (!userInfo?.onboarded) redirect('/onboarding');
+
+	// fetch communities
+	const result = await fetchCommunities({
+		searchString: '',
+		pageNumber: 1,
+		pageSize: 20,
+	});
+
 	return (
 		<section>
 			<h1 className='head-text mb-10'>Communities</h1>
+
+			<div className='mt-14 flex flex-col gap-9'>
+				{result.communities.length === 0 ? (
+					<p className='no-result'>No communities</p>
+				) : (
+					<>
+						{result.communities.map((community) => (
+							<CommunityCard
+								key={community.id}
+								id={community.id}
+								name={community.name}
+								username={community.username}
+								imgUrl={community.image}
+								bio={community.bio}
+								members={community.members}
+							/>
+						))}
+					</>
+				)}
+			</div>
 		</section>
 	);
 };
